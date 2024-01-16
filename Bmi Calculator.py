b3ae5451f82d8a9fd10216e805611c2952f0117e
Kanber Buldu <kanberbuldu@gmail.com>
2024-01-16 12:08:57 +0300

    Completed Bmi Quiz

--- Bmi Calculator.py
+++ Bmi Calculator.py
@@ -42,27 +42,27 @@ def calculate():
         result =round(weight / ((height * 0.01) * (height * 0.01)),2)
         global result_label
         if result < 18.5:
-            result_label.config(text=f"Your Bmi is {result} You are Underweight ", font=FONT)
+            result_label.config(text=f"Your Bmi is {result}. You are Underweight ", font=FONT)
 
         elif result < 24.9:
-            result_label.config(text=f"Your Bmi is {result} You are Normal weight", font=FONT)
+            result_label.config(text=f"Your Bmi is {result}. You are Normal weight", font=FONT)
 
         elif result < 29.9:
-            result_label.config(text=f"Your Bmi is {result} Your are Overweight", font=FONT)
+            result_label.config(text=f"Your Bmi is {result}. Your are Overweight", font=FONT)
 
         elif result < 34.9:
             result_label.config(text=f"Your Bmi is {result} You are Obese (Class 1) ", font=FONT)
         elif result < 40:
-            result_label.config(text=f"Your Bmi is {result} You are Obese (Class 2) ", font=FONT)
+            result_label.config(text=f"Your Bmi is {result}. You are Obese (Class 2) ", font=FONT)
 
         elif result > 40:
-            result_label.config(text=f"Your Bmi is {result} You are Extremly Obese ", font=FONT)
+            result_label.config(text=f"Your Bmi is {result}. You are Extremly Obese ", font=FONT)
 
         else:
-            result_label.config(text="Your weight or height is not valid !", font=FONT)
+            result_label.config(text="Enter Valid Number!", font=FONT)
 
     except ValueError :
-            result_label.config(text="Your weight or height is not valid !", font=FONT)
+            result_label.config(text="Enter Valid Number!", font=FONT)
 
 
 
